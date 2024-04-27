0ee1c7948110fc2c366b5296ebbe7b12d058a4cd
josue1101 <150561866+josue1101@users.noreply.github.com>
2024-04-26 19:29:27 -0600

    j

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const path = require("path");
-const paymentRoutes = require("./routes/payment.routes.js");
 const stripe = require("stripe")(
   "sk_test_51O6QsWJGdC53RqzMKrr5WmubTo6oAGEk05LQN2PgQRZCne8XDI1FpeWbhApsHkEG2MgCHRpEuvPxPpaPUmlnakrX00mgHBPWpo"
 ); // Add your Secret Key Here
@@ -12,7 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(paymentRoutes);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.resolve("src/public")));
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,8 +1,1 @@
-const { Router } = require("express");
-const router = Router();
 
-router.get("/create-order", (req, res) => res.send("creating order"));
-router.get("/success", (req, res) => res.send("success-mp"));
-router.get("/webhook", (req, res) => res.send("webhook"));
-
-module.exports = router;
