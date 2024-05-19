f682a39a572ac251b3bef983e2cbf6846ad0f489
StivoRipper <stivofuertes@gmail.com>
2024-05-19 17:23:33 -0600

    Ya quedo

--- src/index.js
+++ src/index.js
@@ -3,17 +3,30 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const path = require("path");
+const cors = require("cors");
 const paymentRoutes = require("./routes/payment.routes.js");
 const stripe = require("stripe")(
   "sk_test_51O6QsWJGdC53RqzMKrr5WmubTo6oAGEk05LQN2PgQRZCne8XDI1FpeWbhApsHkEG2MgCHRpEuvPxPpaPUmlnakrX00mgHBPWpo"
 ); // Add your Secret Key Here
-const mercadopago = require('mercadopago');
+const mercadopago = require("mercadopago");
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(paymentRoutes);
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(express.static(path.resolve("src/public")));
 
@@ -48,11 +61,13 @@ app.post("/stripe-checkout", async (req, res) => {
     //preguntar direccion en Stripe checkout page
   });
   res.json(session.url);
-  console.log("lineItems:",lineItems)
+  console.log("lineItems:", lineItems);
 });
 
-const PAY_PAL_C = "Ab7FA1ndpItrTMH4iSnpiAfxssFkLKM5-T88H61XWY37npvF2aBiWB8nHjvK_9Rw1YpuYu9uJdtjFd7c";
-const PAY_PAL_S = "EBj-hesgdF_q9YFDRkZ1xnuO3eLN9WjycLOYoWI7ffpbbfVvf3AbKk_x67KlUncikbXz-i-7IikPcS5v";
+const PAY_PAL_C =
+  "Ab7FA1ndpItrTMH4iSnpiAfxssFkLKM5-T88H61XWY37npvF2aBiWB8nHjvK_9Rw1YpuYu9uJdtjFd7c";
+const PAY_PAL_S =
+  "EBj-hesgdF_q9YFDRkZ1xnuO3eLN9WjycLOYoWI7ffpbbfVvf3AbKk_x67KlUncikbXz-i-7IikPcS5v";
 
 const PAYPAL_API = "https://api.sandbox.paypal.com";
 
@@ -80,14 +95,21 @@ app.post("/paypal-checkout", async (req, res) => {
   try {
     const accessToken = await getClientCredentials();
 
-    const total = req.body.items.reduce((total, item) => total + parseFloat(item.price.replace(/[^0-9.-]+/g, "")) * item.quantity, 0);
+    const total = req.body.items.reduce(
+      (total, item) =>
+        total +
+        parseFloat(item.price.replace(/[^0-9.-]+/g, "")) * item.quantity,
+      0
+    );
 
     const order = {
       intent: "CAPTURE",
       purchase_units: [
         {
           items: req.body.items.map((item) => {
-            const unitAmount = Math.round(parseFloat(item.price.replace(/[^0-9.-]+/g, "")) * 100);
+            const unitAmount = Math.round(
+              parseFloat(item.price.replace(/[^0-9.-]+/g, "")) * 100
+            );
             return {
               name: item.title,
               quantity: item.quantity.toString(),
@@ -126,7 +148,9 @@ app.post("/paypal-checkout", async (req, res) => {
       }
     );
 
-    const approveLink = orderResponse.data.links.find(link => link.rel === 'approve');
+    const approveLink = orderResponse.data.links.find(
+      (link) => link.rel === "approve"
+    );
 
     return res.json({ url: approveLink.href });
   } catch (error) {
@@ -136,12 +160,11 @@ app.post("/paypal-checkout", async (req, res) => {
 });
 //----------------------------------------------------------------------------Mercado Pago ------------------------------------------------------------//
 //TESTUSER1621058052 oayUoPfmSO     Mastercard   ,  123 , 11/25
-mercadopago.configure({
-  access_token: 'TEST-8996313867297829-042202-0d9a6d51a43c336cb76b72fa1e640c4d-1237362578'
-});
-app.post('/mercadopago-checkout-pro', async (req, res) => {
+mercadopago.configurations.setAccessToken(
+  "TEST-1567338789016917-102921-70531cc6c0e43143b1e79d88a6be3ed6-1529720876"
+);
+app.post("/mercadopago-checkout-pro", async (req, res) => {
   let preference = {
-    
     items: req.body.items.map((item) => {
       return {
         title: item.title,
@@ -150,23 +173,49 @@ app.post('/mercadopago-checkout-pro', async (req, res) => {
       };
     }),
     back_urls: {
-      success: 'http://localhost:4000/payed.html',
-      failure: 'http://localhost:4000/cancel.html'
+      success: "http://localhost:4000/payed.html",
+      failure: "http://localhost:4000/cancel.html",
+      pending: "http://localhost:4000/payed.html",
     },
-    auto_return: 'approved',
+    auto_return: "approved",
+    notification_url: "https://e1dc-148-204-216-241.ngrok-free.app/webhook",
   };
 
   try {
     let preferenceResult = await mercadopago.preferences.create(preference);
-    
+
     console.log(preferenceResult); // Imprime el resultado
-    res.redirect(preferenceResult.body.init_point); // Redirige al usuario al entorno de pruebas de MercadoPago
+    res.json({ init_point: preferenceResult.body.init_point }); // Retorna el init_point como JSON
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
   }
-  
 });
 
+app.post("/webhook", async function (req, res) {
+  const paymentId = req.query.id;
+
+  try {
+    const response = await fetch(
+      `https://api.mercadopago.com/v1/payments/${paymentId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${mercadopago.configurations.getAccessToken()}`,
+        },
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error: ", error);
+    res.sendStatus(500);
+  }
+});
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("Server is running..."));
